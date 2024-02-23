e825e313ee1b04ddaad361f16ed72390666268ac
Lucidarr <97lucymaina@gmail.com>
2024-02-23 15:49:30 +0300

    Initial commit

---
+++ Lucy_s_code.py
@@ -0,0 +1,13 @@
+# Ask user for their name
+name = input("Enter your name: ")
+
+# Ask user for their age
+age = input("Enter your age: ")
+
+# Ask user for their location
+location = input("Enter your location: ")
+
+# Print personalized message
+print(f"Hello {name}, you are {age} years old and live in {location}.")
+input("Press any Key to Exit")
+
